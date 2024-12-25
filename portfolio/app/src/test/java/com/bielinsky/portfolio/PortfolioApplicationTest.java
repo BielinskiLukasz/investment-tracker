c4948d6424033d76d38bd7d91ff89530abf13c82
Łukasz Paweł Bieliński <lukasz.pawel.bielinski@gmail.com>
2024-12-25 23:54:48 +0100

    Portfolio main with context loads test

---
+++ portfolio/app/src/test/java/com/bielinsky/portfolio/PortfolioApplicationTest.java
@@ -0,0 +1,14 @@
+package com.bielinsky.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PortfolioApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
